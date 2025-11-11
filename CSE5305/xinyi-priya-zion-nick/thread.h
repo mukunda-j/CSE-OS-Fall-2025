48c48c6aaf6e473e12414e168cdc090af0117f26
xilxinyilixil <xinyili0606@gmail.com>
2025-11-11 13:06:38 -0500

    updated thread.h

--- CSE5305/xinyi-priya-zion-nick/thread.h
+++ CSE5305/xinyi-priya-zion-nick/thread.h
@@ -2,10 +2,12 @@
 #define _THREAD_H_
 
 struct thread {
-    int thread_ID;
-    int arrival;
-    int turnaround;
-    int response;
+    int thread_ID;      // job ID
+    int arrival;        // arrival time
+    int turnaround;     // turnaround time
+    int response;       // response time
+    int burst;          // burst time
+    int wait;           // wait time
 };
 
 #endif /* _THREAD_H_ */
